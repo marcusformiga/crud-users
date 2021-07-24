b462d62bd1d05be3c1094a1b60a9ef9dfe5508de
marcusformiga <kiko.antares@gmail.com>
2021-07-24 12:09:59 -0300

    feature: criação do middleware de erro global da aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata'
 import express from 'express'
 import 'express-async-errors'
-import {Request, Response} from 'express'
+import {Request, Response, NextFunction} from 'express'
+import { AppError } from '../errors/AppError'
 
 const app = express()
 const port = 3002
@@ -10,6 +11,18 @@ app.use(express.json())
 
 app.get('/test', (request: Request, response: Response) =>{
     return response.send('Rota de teste')
+})
+app.use((error: Error, request: Request, response: Response, next: NextFunction) =>{
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Erro interno do servidor"
+    })
 })
 
 app.listen(port, () =>{
